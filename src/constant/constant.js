43cd728ea32af650406b5bb3e0dc775febc88016
ZaidAsif <zaid.asif.9082@gmail.com>
2025-02-23 17:29:43 +0500

    changed the production url

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,3 +1,3 @@
 // const DEV_URL = `http://localhost:4000`;
 
-export const BASIC_URL = "https://beneficiary-management-backend-seven.vercel.app"
+export const BASIC_URL = "https://duhacks40-backend.vercel.app"
